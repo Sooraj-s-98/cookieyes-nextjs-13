a9b07b953d56e011e88f802c65856d4ed856e872
sooraj <sooraj@mozilor.com>
2023-02-11 05:04:56 +0530

    added in about page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://cdn-cookieyes.com/client_data/eed94624ff7aff67dfe39b4c/script.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-import Script from "next/script";
-
-
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          id="cookieyes"
-          src="https://cdn-cookieyes.com/client_data/eed94624ff7aff67dfe39b4c/script.js"
-          strategy="lazyOnload"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
