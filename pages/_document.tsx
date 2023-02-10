953c582ad7a1fa316086ecbe9587cc526e0a1f0a
sooraj <sooraj@mozilor.com>
2023-02-10 21:56:11 +0530

    cookieyes added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+
+import Script from "next/script";
+
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-      <Script src="https://cdn-cookieyes.com/client_data/eed94624ff7aff67dfe39b4c/script.js"/>
-        </Head>
+      <Head>
+        <Script
+          id="cookieyes"
+          src="https://cdn-cookieyes.com/client_data/eed94624ff7aff67dfe39b4c/script.js"
+          strategy="lazyOnload"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
