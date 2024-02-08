1c884cce645b4b1a0ef459cc869a06fab6a8aa20
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2024-02-08 13:37:47 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script src="https://scriptstaging.cookieyes.com/client_data/d59241ce3a1a17eaff716a6e/script.js" />
+      <Script src="https://mojoview.com/client_data/43b670d7a54a33157d80142c/script.js" />
       <Component {...pageProps} />
     </>
   );
